f76c0ee07743c788edbe6142e45c087bf223dab5
DaytonGarcia <dayton.garcia@gmail.com>
2017-02-04 21:42:17 -0600

    Corrijo archivo php con referencia erronea, cambio i por I

--- Master/masterpage/menu.php
+++ Master/masterpage/menu.php
@@ -2,6 +2,6 @@
 			<nav id="nav">
 				<ul class="links">
 					<li><a href="/ControlDeIngresos/index.php">Home</a></li>
-					<li><a href="/ControlDeIngresos/Registro/Ingreso.php">REGISTRAR INGRESOS</a></li>
+					<li><a href="/ControlDeIngresos/Registro/ingreso.php">REGISTRAR INGRESOS</a></li>
 				</ul>
 			</nav>
--- Master/masterpage/supermenu.php
+++ Master/masterpage/supermenu.php
@@ -3,7 +3,7 @@
 				<ul class="links">
 					<li><a href="/ControlDeIngresos/index.php">Home</a></li>
 					<li><a href="/ControlDeIngresos/Estudiante/Estudiante.php">GESTIONAR ESTUDIANTES</a></li>
-					<li><a href="/ControlDeIngresos/Registro/Ingreso.php">REGISTRAR INGRESOS</a></li>
+					<li><a href="/ControlDeIngresos/Registro/ingreso.php">REGISTRAR INGRESOS</a></li>
 					<li><a href="/ControlDeIngresos/Reportes/Reporte.php">GENERA REPORTES</a></li>
 					<li><a href="/ControlDeIngresos/logout.php">Cerrar Sesion</a></li>
 				</ul>
